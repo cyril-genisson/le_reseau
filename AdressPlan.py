9f461944d36335ea269c6e1b49b508ef73ebebae
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2023-10-17 14:56:37 +0200

    Première version viable de l'adressage réseau

--- AdressPlan.py
+++ AdressPlan.py
@@ -2,13 +2,14 @@
 #
 # Author: Cyril GENISSON
 # Created: 16/10/2023
-# Updated: 16/10/2023
+# Updated: 17/10/2023
 #
 # Nom du script: CalculMasque.py
 # Description: Permet d'établir un plan d'adressage réseau en fonction
 # de la classe d'adresses IP utilisée, du nombre de sous-réseaux et du
 # nombre de postes dans le sous-réseau.
 #
+#  \* WARNING */    Le travail est encore imcomplet pour avoir un plan d'adressage optimisé     \* WARNING */
 
 ############################################################
 #
@@ -16,7 +17,7 @@
 #
 ############################################################
 # Classe d'IP attribuées
-Ip = "192.168.225.15"
+Ip = "10.0.0.0"
 # Liste de sous-réseaux (Nombre_reseau, Nombre_IP) pour le plan d'adressage
 Networks=[(1, 12), (5, 30), (5, 120), (5, 160)]
 
@@ -38,7 +39,7 @@ IpClass={"A": [[0,0,0,1], [126,255,255,254]], \
 NbHosts=[(32, 1), (31, 2)]
 for k in range(2, 32):
     NbHosts.append((32-k, 2**k - 2))
-print(NbHosts)
+
 ############################################################
 #
 # Définitions des fonctions utiles pour le traitement
@@ -60,15 +61,15 @@ def int2bin(integer):
 
 def ClassDefine(l):
     # Retourne la classe de l'adresse IP (Fonction ok)
-    if Ip[0] >= IpClass["A"][0][0] and Ip[0] <= IpClass["A"][1][0]:
+    if int(l[0]) >= IpClass["A"][0][0] and int(l[0]) <= IpClass["A"][1][0]:
         return "A"
-    elif Ip[0] >= IpClass["B"][0][0] and Ip[0] <= IpClass["B"][1][0]:
+    elif int(l[0]) >= IpClass["B"][0][0] and int(l[0]) <= IpClass["B"][1][0]:
         return "B"
-    elif Ip[0] >= IpClass["C"][0][0] and Ip[0] <= IpClass["C"][1][0]:
+    elif int(l[0]) >= IpClass["C"][0][0] and int(l[0]) <= IpClass["C"][1][0]:
         return "C"
-    elif Ip[0] >= IpClass["D"][0][0] and Ip[0] <= IpClass["D"][1][0]:
+    elif int(l[0]) >= IpClass["D"][0][0] and int(l[0]) <= IpClass["D"][1][0]:
         return "D"
-    elif Ip[0] >= IpClass["E"][0][0] and Ip[0] <= IpCLass["E"][1][0]:
+    elif int[l(0)] >= IpClass["E"][0][0] and int(l[0]) <= IpCLass["E"][1][0]:
         return "E"
     else:
         print("L'adresse réseau IPv4 n'est pas correcte")
@@ -125,21 +126,37 @@ def cidr2mask(cidr):
     chaine += "0"
     return bin2ip(chaine)
 
+# Calcul la somme de 2 nombres binaires
+binary_sum = lambda x,y : bin(int(a, 2) + int(b, 2))
 
+
+# Bon là je en me foule pas trop... Je finirai plus tard pour optimiser l'adressage
+# Très très bof mais fonctionnel.
 def PrintNetwork(network):
-    sumL, sumH = 0, 0
+    sumL, sumH, ip = 0, 0, Ip.split(".")
     for k in range(len(network)):
         sumL += network[k][0]
         sumH += network[k][1]
-    print(f"Attribution d'une adresse de classe {ClassDefine(Ip)} {Ip[0]}.{Ip[1]}.{Ip[2]}.{Ip[3]}")
+
+    print("/******************************************************\\")
+    print("\t\t\tPlan d'adressage")
+    print("\******************************************************/")
+
+    print(f"Attribution d'une adresse de classe {ClassDefine(ip)}: {Ip}")
     print(f"Création de {sumL} sous-réseau(x) pour {sumH} hôtes:")
     for i in range(len(network)):
+        cidr = findcidr(network[i][1])
+        mask = cidr2mask(cidr)
+        ipLan = list(ip)
+        print()
+        print("****************************************************")
+        print(f"\tCIDR: {cidr}\tNETMASK= {mask}")
+        print(f"         Nombre d'hôtes utilisables {NbHosts[32-cidr][1]}")
+        print("****************************************************")
         for j in range(network[i][0]):
-            netmask = MaskNetwork(network[i][1])
-           # print(f"Réseau {}/{netmask}")
-           # print(f"\t Adresse de broadcast {}"}
-           # print(f"\t Première adresse utilisable {}")
-           # print(f"\t Dernière adresse utilisable {}")
-           # print(f"\t Nombre d'hôtes utilisables {}")
-           # print(f"\t Nombre de sous-réseaux {}")
+            print(f"Réseau 10.0.{j}.0/{cidr}")
+            print(f"\t Adresse de broadcast 10.0.{j}.{2**(32-cidr)-1}")
+            print(f"\t Première adresse utilisable 10.0.{j}.1")
+            print(f"\t Dernière adresse utilisable 10.0.{j}.{2**(32-cidr)-2}")
 
+PrintNetwork(Networks)
